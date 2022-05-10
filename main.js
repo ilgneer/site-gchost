7e37d122ceab9adbbef5baf9e865a93f62371dd1
iuugui <freezerbrazil@.com>
2022-05-10 00:30:26 -0300

    versão final

--- main.js
+++ main.js
@@ -5,9 +5,41 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
 
-function showNavOnScroll(){
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
@@ -15,7 +47,7 @@ function showNavOnScroll(){
   }
 }
 
-function showBackToTopButtonOnScroll(){
+function showBackToTopButtonOnScroll() {
   if (scrollY > 500) {
     backToTopButton.classList.add('show')
   } else {
@@ -31,13 +63,10 @@ function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
-
-
 ScrollReveal({
   origin: 'top',
-  distance:'30px',
-  duration: 700,
-
+  distance: '30px',
+  duration: 700
 }).reveal(`
   #home,
   #home img,
@@ -47,4 +76,4 @@ ScrollReveal({
   #services .card,
   #about,
   #about header,
-  #about content`);
+  #about content`)
